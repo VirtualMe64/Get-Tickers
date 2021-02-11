daf4284d0adae0782120f81787743a4c31e2d3a9
VirtualMe64 <43835028+VirtualMe64@users.noreply.github.com>
2021-02-11 12:40:23 -0500

    Added constants file, small changes

---
+++ constants.py
@@ -0,0 +1,13 @@
+TICKER_BLACKLIST = ["FOR", "DD", "EV", "CEO", "EOD", "RH", "TD", "HUGE", "IT", "AM", "IMO", "UK"]
+
+MOVES_TOMORROW_ULRS = [
+    "https://www.reddit.com/r/wallstreetbets/comments/lh3qli/what_are_your_moves_tomorrow_february_11_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/lgdb2j/what_are_your_moves_tomorrow_february_10_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/lflpoz/what_are_your_moves_tomorrow_february_09_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/levf1j/what_are_your_moves_tomorrow_february_08_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/lcq65y/what_are_your_moves_tomorrow_february_05_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/lby4jj/what_are_your_moves_tomorrow_february_04_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/lb6aiq/what_are_your_moves_tomorrow_february_03_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/ladzdt/what_are_your_moves_tomorrow_february_02_2021/",
+    "https://www.reddit.com/r/wallstreetbets/comments/l9lvrn/what_are_your_moves_tomorrow_february_01_2021/",
+]
